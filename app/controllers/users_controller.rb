e5b4ec972de9d2e52bd69bc5b82dc4c2d5b49ac4
Rachel Miller <raeanne1991@gmail.com>
2018-03-01 00:53:19 +0000

    edited user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   use Rack::Flash
 
   get '/signup' do
-    if !session[:user_id]
-      erb :'users/new'
+    if logged_in?
+      redirect to '/packs'
     else
-      redirect to '/packs/new'
+      erb :'users/new'
     end
   end
 
@@ -27,33 +27,35 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-    if !session[:user_id]
-      erb :'users/new'
+    if !logged_in?
+      erb :'/'
     else
       redirect to '/packs'
     end
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/packs"
+    @user = User.find_by(username: params[:username])
+    if params[:username] == "" || params[:password] == ""
+      flash[:message] = "You have left one or more fields blank. Please try again."
+      redirect '/login'
+    elsif @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/packs'
     else
-      redirect to '/signup'
+      flash[:message] = "You have entered an incorrect password/username. Please try again, or create an account."
+      redirect to '/'
     end
   end
 
   get '/users/:slug' do
-    if !logged_in?
-      redirect '/signup'
-    end
-
-    @user = User.find_by_slug(params[:slug])
-    if !@user.nil? && @user == current_user
-      erb :'users/show'
+    if logged_in?
+      @user = User.find_by_slug(params[:slug])
+      @packs = @user.packs
+      @packs.order! 'created_at DESC'
+      erb :'/users/show'
     else
-      redirect '/packs'
+      redirect '/'
     end
   end
 
